12e152c1886a623f2902ce31a6f0cf1a8c109813
Georg <https://github.com/GeorgeBrat/helloworld.git>
2024-04-10 23:35:16 +0300

    HW12 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Author author1 = new Author("Джордж", "Оруэлл");
+    Book book1 = new Book("1984", 1949,author1);
+    Author author2 = new Author("Джек", "Лондон");
+    Book book2 = new Book("Мартин Иден", 2024,author2);
+
+        System.out.println("Год публикации: " + book2.getPublicationYear());
+
+        book2.setPublicationYear(1909);
+        System.out.println("Год публикации: " + book2.getPublicationYear());
+        System.out.println("Автор: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName());
     }
 }
